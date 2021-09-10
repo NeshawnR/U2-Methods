9396646f9ef1234435ec2be07ec488b95e716626
NeshawnR <richane000@warren.k12.in.us>
2021-09-10 13:07:32 -0400

    U2 L1,2

--- L1-Methods Without Parameters/src/CircleCalc.java
+++ L1-Methods Without Parameters/src/CircleCalc.java
@@ -1,14 +1,35 @@
+import javax.swing.*;
+import java.text.DecimalFormat;
+
 public class CircleCalc {
 
 
     public static void main(String[] args) {
 
         //call the area method
-
+    area();
         //call the circumference method
+    circumference();
+    }
+    public static void area(){
+
+        DecimalFormat round = new DecimalFormat("#.##");
+        double radius = Double.parseDouble(JOptionPane.showInputDialog("What is the radius?"));
+
+        double area = Math.PI * Math.pow(radius, 2);
+
+        JOptionPane.showMessageDialog(null, "The area is " + round.format(area));
+}
+
+    public static void circumference(){
+        DecimalFormat round = new DecimalFormat("#.##");
+        double radius = Double.parseDouble(JOptionPane.showInputDialog("What is the radius?"));
+
+        double circumference = 2 * Math.PI * radius;
+
+        JOptionPane.showMessageDialog(null, "The circumference is " + round.format(circumference));
 
     }
-
     //write a method to calculate a circle's area
 
 
--- L2-Methods with Parameters/src/CircleCalc.java
+++ L2-Methods with Parameters/src/CircleCalc.java
@@ -1,15 +1,34 @@
+import javafx.beans.binding.Bindings;
+
+import javax.swing.*;
+import java.text.DecimalFormat;
+
 public class CircleCalc {
 
 
+
     public static void main(String[] args) {
-
+        double radius = Double.parseDouble(JOptionPane.showInputDialog("What is the radius?"));
         //create and run the program
-
+    area(radius);
+    circumference(radius);
     }
 
     //re-write the method to calculate a circle's area using parameters
+    public static void area(double radius){
+    DecimalFormat round = new DecimalFormat("#.##");
+    double area = Math.PI * Math.pow(radius, 2);
+
+    JOptionPane.showMessageDialog(null, "The area is " + round.format(area));
+}
+
 
 
     //re-write the method to calculate a circle's perimeter using parameters
+    public static void circumference(double radius){
+    DecimalFormat round = new DecimalFormat("#.##");
+    double circumference = 2 * Math.PI * radius;
 
+    JOptionPane.showMessageDialog(null, "The circumference is " + round.format(circumference));
+    }
 }
