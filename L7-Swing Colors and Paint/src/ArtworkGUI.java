f524ec32bf2f63b8951191016c87bd5b10bc32cc
NeshawnR <richane000@warren.k12.in.us>
2021-10-27 12:40:23 -0400

    U2 L3

--- L7-Swing Colors and Paint/src/ArtworkGUI.java
+++ L7-Swing Colors and Paint/src/ArtworkGUI.java
@@ -1,20 +1,48 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class ArtworkGUI {
 
     //DECLARE VARIABLES
+    JFrame frame;
 
-    public ArtworkGUI()
-    {
+    public ArtworkGUI() {
         //CREATE THE GUI
+        frame = new JFrame("Art Work");
+        frame.setSize(600, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+        JPanel panel = new MyDrawing();
+
+        frame.add(panel);
+        frame.setVisible(true);
     }
 
-    private class MyDrawing extends JPanel
-    {
+    private class MyDrawing extends JPanel {
+
         public MyDrawing(){
             //Set the background color
+            setBackground(Color.CYAN);
         }
 
+
+
         //Add the PaintComponent Method and create your drawing.
+        public void paintComponent(Graphics g){
+
+            super.paintComponent(g);
+
+            g.setColor(java.awt.Color.green);
+            g.fillRect(0, 500, 700, 100);
+
+            g.setColor(Color.YELLOW);
+            g.fillRect(140, 200, 300, 300);
+
+            g.setColor(Color.red);
+            g.fillRect(110, 100, 360, 100);
+
+
+        }
     }
 }
